d563d2d55db4162d231590c489c5a31adbff6818
Mislav Glavačević <34093897+mislavglavacevic@users.noreply.github.com>
2018-09-05 12:09:05 +0200

    Update slika.php

--- slika.php
+++ slika.php
@@ -1,16 +1,46 @@
 <?php
+ 
 
-$stamp = imagecreatefrompng('stamp.png');
-$im = imagecreatefromjpeg('photo.jpeg');
+header("Content-type: image/png"); 
+ 
 
-$marge_right = 10;
-$marge_bottom = 10;
-$sx = imagesx($stamp);
-$sy = imagesy($stamp);
+$width = 350;
+$height = 360;
+ 
 
-imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+$im = ImageCreateTrueColor($width, $height); 
+ 
 
-header('Content-type: image/png');
-imagepng($im);
-imagedestroy($im);
+ImageAntiAlias($im, true);
+ 
+
+$white = ImageColorAllocate($im, 255, 255, 255); 
+ImageFillToBorder($im, 0, 0, $white, $white);
+ 
+
+$black = ImageColorAllocate($im, 0, 0, 0);
+$blue = ImageColorAllocate($im, 0, 0, 255);
+ 
+
+$r_width = 150;
+$r_height = 100;
+$r_x = 100;
+$r_y = 50;
+ 
+ImageRectangle($im, $r_x, $r_y, $r_x+$r_width, $r_y+$r_height, $black);
+ 
+
+$r_width = 150;
+$r_height = 100;
+$r_x = 100;
+$r_y = 200;
+ 
+ImageFilledRectangle($im, $r_x, $r_y, $r_x+$r_width, $r_y+$r_height, $blue);
+ 
+
+ImagePNG($im); 
+ 
+
+ImageDestroy($im); 
+ 
 ?>
