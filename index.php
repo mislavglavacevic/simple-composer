71d9e99352f383b8a294f7148bdc879bc95d9ffb
Mislav Glavačević <34093897+mislavglavacevic@users.noreply.github.com>
2018-09-05 12:18:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require 'vendor/autoload.php';
 
 use Intervention\Image\ImageManagerStatic as Image;
 
-$image = Image::make('img/shape.jpg')->resize(300, 200)->save("img/shape-2.jpg", 100);
+$image = Image::make('img/slika.jpg')->resize(300, 200)->save("img/slika-2.jpg", 100);
 
-echo '<img src="img/shape-2.jpg" alt=""/>';
+echo '<img src="img/slika-2.jpg" alt=""/>';
 
 ?>
