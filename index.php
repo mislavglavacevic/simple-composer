9969afbface388256da0fff5b4597a280a879574
Mislav Glavačević <34093897+mislavglavacevic@users.noreply.github.com>
2018-09-07 11:25:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,24 +4,32 @@ require 'vendor/autoload.php';
 
 use Intervention\Image\ImageManagerStatic as Image;
 
-
-$image = Image::canvas(300, 200, '#ddd');
-
-
-$image->circle(100, 50, 50, function ($draw) {
-    $draw->background('#0000ff');
-});
-
-
-$image->circle(10, 100, 100, function ($draw) {
-    $draw->background('#0000ff');
-    $draw->border(1, '#f00');
-});
+$mjera = array(
+            10,  10,  
+            50,  40, 
+            240,  200,  
+            60, 60,  
+            20,  240,  
+            40,  50   
+            );
 
 
-$image->circle(70, 150, 100, function ($draw) {
-    $draw->border(5, '000000');
-});
+$slika = imagecreatetruecolor(250, 250);
+
+
+$pozadina   = imagecolorallocate($slika, 0, 0, 255);
+$boja = imagecolorallocate($slika, 255, 0, 0);
+
+
+imagefilledrectangle($slika, 0, 0, 249, 249, $pozadina);
+
+
+imagefilledpolygon($slika, $mjera, 6, $boja);
+
+
+header('Content-type: image/png');
+imagepng($slika);
+imagedestroy($slika);
 
 $image = Image::make('img/slika.jpg')->resize(300, 200)->save("img/slika-2.jpg", 100);
 
